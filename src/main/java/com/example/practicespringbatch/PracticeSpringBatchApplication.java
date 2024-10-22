a927388455ed477e03cc8bc237b6a12c4e5f7b5c
wlsgur073 <wlsgur073@gmail.com>
2024-10-22 15:35:00 +0900

    Let's start to make a simple spring batch project!

---
+++ src/main/java/com/example/practicespringbatch/PracticeSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicespringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringBatchApplication.class, args);
+    }
+
+}
